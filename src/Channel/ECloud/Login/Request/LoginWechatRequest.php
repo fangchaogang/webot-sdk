4475f897a1b56d08f1ed26302f28b0878787dfa3
方朝罡 <fangcg@kzmall.cn>
2025-08-26 10:06:59 +0800

    fix

--- src/Channel/ECloud/Login/Request/LoginWechatRequest.php
+++ src/Channel/ECloud/Login/Request/LoginWechatRequest.php
@@ -11,6 +11,7 @@ use Webot\Channel\ECloud\Request;
  * @package Webot\Channel\ECloud\Login\Request
  * @method $this setWId($wId)
  * @method $this setVerifyCode($verifyCode)
+ * @method $this setAutoCheck($autoCheck)
  */
 class LoginWechatRequest extends Request
 {
@@ -23,6 +24,10 @@ class LoginWechatRequest extends Request
      *
      * setVerifyCode($verifyCode)
      * 非必填：验证码 默认不传，若扫码结束后，本接口返回提示"请在ipad上输入验证码"，则再调用1次本接口且传验证码即可（PS：极少情况下会出现此情况,可忽略此字段）
+     *
+     * setAutoCheck($autoCheck)
+     * 非必填：只针对mac协议，建议填false
+     *
      */
 
     /**
